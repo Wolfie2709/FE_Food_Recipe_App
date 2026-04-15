2210b03d923e8c53c6202764f97c0ba516e04985
WOLFIE-PC69\PC <klodfghr19@gmail.com>
2026-04-15 12:05:37 +0700

    added new types

--- types.ts
+++ types.ts
@@ -17,14 +17,14 @@ export type User = {
 export type LoginRequest = {
     username: string;
     password: string;
-}
+};
 
 export type LoginResponse = {
     username: UserInfo;
     role: string;
     expiryTime: number;
     access_token: string;
-}
+};
 
 export type RegisterRequest = {
     first_name: string;
@@ -35,7 +35,7 @@ export type RegisterRequest = {
     email: string;
     sex: string;
     birthdate?: string;
-}
+};
 
 export type UserInfo = {
     userId: number;
@@ -43,4 +43,59 @@ export type UserInfo = {
     role?: string;
   };
 
-  
+export type ChangePasswordDTO = {
+    username: string;
+    oldPassword: string;
+    newPassword: string;
+ };
+
+export type Ingredient = {
+    id: number;
+    name: string;
+    categoryId: number;
+    categoryName: string;
+
+ };
+export type RecipeBoxDTO = {
+    id: number;
+    name: string;
+    addedBy: string;
+    rating: number;
+    imageUrl: string;
+};
+export type Recipe = {
+    id: number;
+    name: string;
+    cookingTime: number;
+    servingSize: number;
+ };
+export type KitchenUtensils = {
+    id: number;
+    name: string;
+    imageUrl: string;
+ };
+
+export type Review = {
+    username: string;
+    image: string;
+    content: string;
+    rating: number;
+    date: Date;
+};
+
+export type Wishlist = {
+    id: number;
+
+};
+
+export type ShoppingList = {
+
+};
+
+export type Category = {
+    
+};
+
+export type Image= {
+
+}
