a77576742cd743a4e96a41bd952d79d8d0dc0826
WOLFIE-PC69\PC <klodfghr19@gmail.com>
2026-06-11 11:33:20 +0700

    Fix bugs and added a new categoryDroplist

--- utils/apiConfig.tsx
+++ utils/apiConfig.tsx
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
     local_emulator: "http://10.0.2.2:5103/",
     phone_test_laptop: "http://192.168.38.220:5103/",
-    phone_test: "http:///192.168.1.100:5103/",
+    phone_test: "http:///192.168.100.169:5103/",
     HSU_test: "http://10.106.34.36/"
 };
 
